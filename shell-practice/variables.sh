5f386dc8161a5cf8c92f1d381d2d606f47fd1277
bhaskar <bhaskarraochandhaka@gmail.com>
2026-02-14 11:48:55 +0530

    changes added

--- shell-practice/variables.sh
+++ shell-practice/variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-person_1=$pinky
-person_2=$rajesh
+person_1="pinky"
+person_2="rajesh"
 
 
-echo "person_1:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
-echo "person_2:I am doing great,what are you doing currently?"
-echo "person_1:I am learning devops ,What about you ?"
-echo "person_2:oh great i am also learning devops"
+echo "$person_1:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
+echo "$person_2:I am doing great,what are you doing currently?"
+echo "$person_1:I am learning devops ,What about you ?"
+echo "$person_2:oh great i am also learning devops"
