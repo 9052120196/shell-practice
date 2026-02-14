a5a222ff33ef7432ede3f29c18f01957661f0673
bhaskar <bhaskarraochandhaka@gmail.com>
2026-02-14 10:29:30 +0530

    error fixing

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@
 echo "raju:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
 echo "rajesh:I am doing great,what are you doing currently?"
 echo "raju:I am learning devops ,What about you ?"
-echo "rajesh:"oh great i am also learning devops"
+echo "rajesh:oh great i am also learning devops"
 
 
 
