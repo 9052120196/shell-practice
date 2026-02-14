6257d7a36e4d6915aea7ffb99a911079f273e206
bhaskar <bhaskarraochandhaka@gmail.com>
2026-02-14 10:37:09 +0530

    replaced

--- variables.sh
+++ variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "raju:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
-echo "rajesh:I am doing great,what are you doing currently?"
-echo "raju:I am learning devops ,What about you ?"
-echo "rajesh:oh great i am also learning devops"
+echo "rani:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
+echo "swetha:I am doing great,what are you doing currently?"
+echo "rani:I am learning devops ,What about you ?"
+echo "swetha:oh great i am also learning devops"
 
 
 
