7241b32d32fbde18320e789b2aaf9a13a1d3b8e3
bhaskar <bhaskarraochandhaka@gmail.com>
2026-02-14 11:24:03 +0530

    variables added

--- shell-practice/variables.sh
+++ shell-practice/variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+person_1=$pinky
+person_2=$rajesh
 
-echo "rani:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
-echo "swetha:I am doing great,what are you doing currently?"
-echo "rani:I am learning devops ,What about you ?"
-echo "swetha:oh great i am also learning devops"
+
+echo "person_1:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
+echo "person_2:I am doing great,what are you doing currently?"
+echo "person_1:I am learning devops ,What about you ?"
+echo "person_2:oh great i am also learning devops"
