9beb98288d7c498cfacb949e5455429049e1460a
bhaskar <bhaskarraochandhaka@gmail.com>
2026-04-05 16:12:25 +0530

    functional

--- shell-practice/installation.sh
+++ shell-practice/installation.sh
@@ -2,8 +2,29 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-  echo "please run the script with root user access"
+  echo "Ngnix installation is success"
+   else
+      echo "Nginx installation is failed"
   fi
 
 echo "installing nginx"
 dnf install nginx -y
+
+if [ $USERID -ne 0 ]; then
+   echo "Nodejs installation is success"
+  else
+     echo "Nodejs installation is failed"
+  
+  fi
+
+echo "installing nodejs"
+dnf install nodejs -y
+
+if [ $USERID -ne 0 ]; then
+  echo "mysql installation is success"
+  else
+  echo "mysql installation is failed"
+  fi
+
+echo "installing mysql"
+dnf install mysql -y
