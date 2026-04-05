5c3ce28a8bf560cdfa7c5f2665b99aea5d06905d
bhaskar <bhaskarraochandhaka@gmail.com>
2026-04-05 12:38:56 +0530

    special variables

---
+++ shell-practice/variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person_1="$1"
+person_2="$2"
+
+
+echo "$person_1:I ramana how are you ?I am doing well thanks for asking .How do you do ?"
+echo "$person_2:I am doing great,what are you doing currently?"
+echo "$person_1:I am learning devops ,What about you ?"
+echo "$person_2:oh great i am also learning devops"
